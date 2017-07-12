654f1099333c2b409f6666a69d53e6bc3aa2c601
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-07-12 18:07:45 +0200

    Added ssh and rsync commands for the node type

--- node.go
+++ node.go
@@ -1,13 +1,18 @@
 package sctl
 
-import "database/sql"
-
+import (
+	"fmt"
+	
+	"database/sql"
+)
+	
 // Node contains node metadata
 type Node struct {
 	Project  string `json:"project"`
 	IP       string `json:"ip"`
 	OS       string `json:"os"`
 	IsMaster bool   `json:"isMaster"`
+	User	 string `json:"user"`
 }
 
 //SetToMinion sets a node struct to hold values of a minion
@@ -24,6 +29,29 @@ func (node *Node) SetToMinion(db *sql.DB) error {
 	return nil
 }
 
+// SSHCommand Creates an ssh command to be executed on the node
+func (node Node) SSHCommand(args ...string) Command {
+	target := []string{node.Remote()}
+	return Command{
+		Main: "ssh",
+		Args: append(target, args...),
+	}
+}
+
+// RsyncCommand Creates a command to rsync minion executables to the node
+func (node Node) RsyncCommand(execFolder string) Command {
+	target := fmt.Sprintf("%s:/etc/init.d/", node.Remote())
+	return Command{
+		Main: "rsync",
+		Args: []string{execFolder, target}
+	}
+}
+
+// Remote Creates a remote node adress such as user@hostname
+func (node Node) Remote() string {
+	return fmt.Sprintf("%s@%s", node.User, node.IP)
+}
+
 // NodeSchema Returns the database schema for nodes
 func NodeSchema() string {
 	return `CREATE TABLE NODE(
@@ -31,6 +59,7 @@ func NodeSchema() string {
       IP VARCHAR(50),
       OS VARCHAR(10) DEFAULT 'linux',
       IS_MASTER BOOLEAN,
+      USER VARCHAR(40),
       FOREIGN KEY (PROJECT) REFERENCES PROJECT(NAME),
       PRIMARY KEY (PROJECT, IP)
     )`
