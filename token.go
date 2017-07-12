eb170080b5959b7949159411319d8d734350674e
Simon Lindgren <simon.g.lindgren@gmail.com>
2017-07-12 18:42:11 +0200

    Created token type, new token func and valid time check

--- token.go
+++ token.go
@@ -8,6 +8,28 @@ import (
 	"time"
 )
 
+// Token Struct to keep a token and its timestamp
+type Token struct {
+	Data      string    `json:"data"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+// Valid checks if a token in valid or out of date
+type (token Token) Valid() bool {
+	var maxAge float64 = 300.0
+	now := time.Now().UTC()
+	difference := now.Sub(token.Timestamp)
+	return difference.Seconds() <= maxAge
+}
+
+// NewToken Return a new random token with timestamp set to the current UTC time
+func NewToken(length int) Token {
+	return Token{
+		Data:      GenrateToken(length),
+		Timestamp: time.Now().UTC().
+	}
+}
+
 // GenerateToken Creates a random token consiting of aspecified number of sha256 hashes
 func GenerateToken(length int) string {
 	tokenParts := make([]string, 0)
@@ -23,4 +45,14 @@ func TokenPart() string {
 	rand.Seed(time.Now().UnixNano())
 	rand.Read(randBytes)
 	return fmt.Sprintf("%x", sha256.Sum256(randBytes))
+}
+
+// TokenSchema Returns the token schema
+func TokenSchema() string {
+	return `CREATE TABLE TOKEN(
+		AUTH VARCHAR(64)
+		MASTER VARCHAR(260)
+		AUTH_TIMESTAMP TIMESTAMP
+	)		
+	`
 }
